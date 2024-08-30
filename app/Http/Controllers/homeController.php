ddc73a8d13145d5138ef9b86e95b144f37d1dabe
Arthur-Henrique-Student-UnC <139513834+Arthur-Henrique-Student-UnC@users.noreply.github.com>
2024-08-30 16:31:03 -0300

    Última aula sobre php laravel

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -8,7 +8,18 @@ class homeController extends Controller
 {
     public function index()
     {
-        
-        return view('home');
+        $data = [
+            'pageTitle' => 'Exemplo',
+            'showButton' => true,
+            'items' => ['Item 1', 'Item 2', 'Item 3'],
+            'user' => ['name' => 'Arthur Ribeiro Henrique', 'role' => 'Aluno']
+        ];
+        return view('home', $data);
     }
+
+    public function about(){
+        return view('about');
+    }
+
+
 }
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,14 +1,15 @@
-<!DOCTYPE html>
+/<!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> </title>
-    <link rel="stylesheet" href="">
+    <title>Pilot_Project</title>
+    <!--Asset: Algo de valor-->
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
     <header>
-        <h1> </h1>
+        <h1>{{ $pageTitle }}</h1>
         <nav>
             <ul>
                 <li><a href="#">Home</a></li>
@@ -19,11 +20,11 @@
     </header>
 
     <main>
-        
+        @yield('content')
     </main>
 
     <footer>
-        <p>&copy;  Minha Empresa</p>
+        <p>&copy;  {{ date('Y')}} Minha Empresa</p>
     </footer>
 
     <script src=""></script>
